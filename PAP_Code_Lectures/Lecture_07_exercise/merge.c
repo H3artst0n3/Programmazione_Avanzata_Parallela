4fd50fedb0582ef27db1ccc0be9b4fab664dc4b7
Angie <angirota@gmail.com>
2023-10-18 10:09:59 +0200

    Fixed all, I hope

---
+++ PAP_Code_Lectures/Lecture_07_exercise/merge.c
@@ -0,0 +1,35 @@
+#include "merge.h"
+
+void merge(int * v1, int n1, int * v2, int n2, int * results)
+{
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  while ((i < n1) && (j < n2)){
+    if (v1[i] < v2[j]){
+      results[k] = v1[i];
+      i++;
+    }else{
+      results[k] = v2[j];
+      j++;
+    }
+    k++;
+  }
+}
+
+
+void merge_branchless(int * v1, int n1, int * v2, int n2, int * results)
+{
+  int i = 0;
+  int j = 0;
+  int k = 0;
+
+  while ((i < n1) && (j < n2)){
+    while((k < (n1 +n2))){
+      int q = v1[i] < v2[j];
+
+      k++;
+    }
+  }
+}
