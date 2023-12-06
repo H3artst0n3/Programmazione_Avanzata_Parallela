07ea3ee1a699a3be83ce46497a1d53a88513ba87
Angie <angirota@gmail.com>
2023-12-06 20:18:55 +0100

    modified Lecture 16 and 24

--- PAP_Code_Lectures/Lecture_24_exercise/tree.py
+++ PAP_Code_Lectures/Lecture_24_exercise/tree.py
@@ -3,6 +3,7 @@ from ctypes import cdll, c_bool, c_float, c_int, pointer, POINTER, Structure
 
 
 class CTreeNode(Structure):
+    # this must be empty
     pass
 
 
@@ -17,25 +18,53 @@ class CTree:
     code = None
 
     def __init__(self):
+        # if we don't have a tree then initialize it
         if CTree.code is None:
-            CTree.code = cdll.LoadLibrary("libtree.dll")
+            # import the C library
+            CTree.code = cdll.LoadLibrary("./libtree.dll")
+            # we have to define the types of the parameters of insert
             CTree.code.insert.argtypes = [POINTER(CTreeNode), c_int, c_float]
+            # we have to define the type of the value returned by insert
             CTree.code.insert.restype = POINTER(CTreeNode)
-            CTree.code.search.argtypes = [POINTER(CTreeNode),
-                                          c_int, POINTER(c_float)]
+            # we have to define the types of the parameters of search
+            CTree.code.search.argtypes = [POINTER(CTreeNode), c_int, POINTER(c_float)]
+            # we have to define the type of the value returned by search
             CTree.code.search.restype = c_bool
+            # we have to define the types of the parameters of destroy
             CTree.code.destroy.argtypes = [POINTER(CTreeNode)]
+        # if we have a root set it like a pointer
         self.root = POINTER(CTreeNode)()
 
     def search(self, key):
-        pass
-
+        # ??
+        res = c_float()
+        # ??
+        found = CTree.code.search(self.root, c_int(key), pointer(res))
+        # if found = True
+        if found: 
+            # return the value
+            return res.value
+        
+        # if we didn't find the key return None
+        return None
+    
     def insert(self, key, value):
-        pass
+        # insert the value and the key in the right subtree
+        self.root = CTree.code.insert(self.root, c_int(key), c_float(value))
 
     def __str__(self):
-        pass
-
+        def print_node(node):
+            if not node:
+                return "None"
+            s = f"{node.contents.key}"
+            if node.contents.left or node.contents.right:
+                left = print_node(node.contents.left)
+                right = print_node(node.contents.right)
+                s = f"({s} {left} {right})"
+            return s
+        
+        return print_node(self.root)
+    
     def __del__(self):
         CTree.code.destroy(self.root)
 
@@ -49,29 +78,46 @@ class Node:
         self.left = None
 
     def search(self, key):
+        # check if the key is in the current node
         if self.key == key:
-            return self.value
-        elif (self.key < key) and (not self.right == None):
+            return self.value # return the value of the key
+        
+        # go right and check if we have a child or a None value
+        if (self.key < key) and (self.right is not None):
             return self.right.search(key)
-        elif (self.key > key) and (not self.left == None):
+        
+        # go left and check if we have a child or a None value
+        if (self.key > key) and (self.left is not None):
             return self.left.search(key)
         
         return None
 
     def insert(self, key, value):
+        # if the key is already in the current node
         if self.key == key:
-            self.value = value
-        elif (self.key < key) and (self.right == None):
-            self.right = Node(key, value)
-        elif (self.key > key) and (self.left == None):
-            self.left = Node(key, value)
-        elif (self.key < key) and (not self.right == None):
-            self.right.insert(key, value)
-        elif (self.key > key) and (not self.left == None):
-            self.left.insert(key, value)
+            self.value = value # assign the new value
+        
+        # go right
+        if self.key < key:
+            # if there is no child initialize a new node
+            if self.right is None:
+                self.right = Node(key, value)
+            else: # call insert on the right child
+                self.right.insert(key, value) 
+        
+        # go left
+        if self.key > key:
+            # if there is no child initialize a new node
+            if self.left is None:
+                self.left = Node(key, value)
+            else: # call insert on the left child
+                self.left.insert(key, value)
 
     def __str__(self):
-        return f"({self.key} {self.left} {self.right})"
+        s = f"{self.key}"
+        if self.right is not None or self.left is not None:
+            s = f"({s} {str(self.left)} {str(self.right)})"
+        return s
 
 
 class BinarySearchTree:
@@ -80,15 +126,17 @@ class BinarySearchTree:
         self.root = None
 
     def search(self, key):
-        if not self.root is None:
+        # if we have a root do the search
+        if self.root is not None:
             return self.root.search(key)
         
         return None
 
     def insert(self, key, value):
-        if not self.root is None:
+        # if we have a root do the insert
+        if self.root is not None:
             self.root.insert(key, value)
-        else:
+        else: # if we don't have a root initialize it like a Node with the key and value of the insert function
             self.root = Node(key, value)
 
     def __str__(self):
@@ -105,10 +153,10 @@ if __name__ == "__main__":
     print(bst)
 
     # Decommentare per la parte extra
-    # cbst = CTree()
-    # for i in range(0, 20):
-    #     cbst.insert(random.randint(0, 30), random.random())
-    # for i in range(0, 10):
-    #     k = random.randint(0, 30)
-    #     print(f"Searching for {k}: {cbst.search(k)}")
-    # print(cbst)
+    cbst = CTree()
+    for i in range(0, 20):
+        cbst.insert(random.randint(0, 30), random.random())
+    for i in range(0, 10):
+        k = random.randint(0, 30)
+        print(f"Searching for {k}: {cbst.search(k)}")
+    print(cbst)
