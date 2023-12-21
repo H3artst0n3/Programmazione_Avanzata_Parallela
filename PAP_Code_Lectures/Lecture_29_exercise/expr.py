5bffec7bafc777bb9b7426a0423636429fed9b15
Angie <angirota@gmail.com>
2023-12-21 16:12:33 +0100

    modified lecture 29 exercise

--- PAP_Code_Lectures/Lecture_29_exercise/expr.py
+++ PAP_Code_Lectures/Lecture_29_exercise/expr.py
@@ -37,21 +37,27 @@ class Expression:
                 if issubclass(op_class, Operation):
                     if op_class.arity == 1:
                         arg = stack.pop()
-                        stack.push(op_class(arg))
+                        stack.push(op_class([arg]))
                     elif op_class.arity == 2:
                         arg2 = stack.pop()
                         arg1 = stack.pop()
-                        op_class = op_class([arg1, arg2])
-                        stack.push(op_class.op(arg1, arg2))
+                        stack.push(op_class([arg1, arg2]))
                 else:
                     stack.push(op_class())
             else:
-                stack.push(Constant(token))
+                try:
+                    value = int(token)
+                    stack.push(Constant(value))
+                except ValueError:
+                    stack.push(Variable(token))
 
         return stack.pop()
-    
+
     def evaluate(self, env):
-        raise NotImplementedError()
+        raise NotImplementedError("Subclass must implement evaluate method")
+    
+    def __str__(self):
+        raise NotImplementedError("Subclass must implement __str__ method")
 
 
 class MissingVariableException(Exception):
@@ -67,7 +73,7 @@ class Variable(Expression):
         if self.name in env:
             return env[self.name]
         else:
-            raise MissingVariableException
+            raise MissingVariableException()
 
     def __str__(self):
         return self.name
@@ -83,13 +89,6 @@ class Constant(Expression):
 
     def __str__(self):
         return str(self.value)
-    
-    def __add__(self, y):
-        return self.value + y.value
-    
-    def __mul__(self, y):
-        self.value *= y.value
-        return self.value
 
 
 class Operation(Expression):
@@ -98,10 +97,11 @@ class Operation(Expression):
         self.args = args
 
     def evaluate(self, env):
-        pass
+        evaluated_args = [arg.evaluate(env) for arg in self.args]
+        return self.op(*evaluated_args)
 
     def op(self, *args):
-        raise NotImplementedError()
+        raise NotImplementedError("Subclass must implement op method")
 
     def __str__(self):
         pass
@@ -109,81 +109,73 @@ class Operation(Expression):
 
 class BinaryOp(Operation):
     arity = 2
-    pass
-
 
 class UnaryOp(Operation):
     arity = 1
-    pass
 
 
 class Addition(BinaryOp):
-    def op(self, x, y):
-        return x + y
-    
+    def op(self, *args):
+        return args[1] + args[0]
+
     def __str__(self):
-        return f"(+ {self.args[0]} {self.args[1]} )"
+        return f"(+ {self.args[1]} {self.args[0]})"
 
 
 class Subtraction(BinaryOp):
-    def op(self, x, y):
-        return x - y
-    
+    def op(self, *args):
+        return args[1] - args[0]
+
     def __str__(self):
-        return f"({self.args[0]} - {self.args[1]})"
+        return f"(- {self.args[1]} {self.args[0]})"
 
 
 class Division(BinaryOp):
-    def op(self, x, y):
-        return x / y
-    
-    def __str__(self):
-        return f"({self.args[0]} / {self.args[1]})"
+    def op(self, *args):
+        return args[1] / args[0]
 
+    def __str__(self):
+        return f"(/ {self.args[1]} {self.args[0]})"
 
 
 class Multiplication(BinaryOp):
-    def op(self, x, y):
-        return x * y
-    
-    def __str__(self):
-        return f"({self.args[0]} {self.args[1]} * )"
+    def op(self, *args):
+        return args[1] * args[0]
 
+    def __str__(self):
+        return f"(* {self.args[1]} {self.args[0]})"
 
 
 class Power(BinaryOp):
-    def op(self, x, y):
-        return x ** y
-    
-    def __str__(self):
-        return f"({self.args[0]} ** {self.args[1]})"
+    def op(self, *args):
+        return args[1] ** args[0]
 
+    def __str__(self):
+        return f"(** {self.args[1]} {self.args[0]})"
 
 
 class Modulus(BinaryOp):
-    def op(self, x, y):
-        return x % y
-    
-    def __str__(self):
-        return f"({self.args[0]} % {self.args[1]})"
+    def op(self, *args):
+        return args[1]%args[0]
 
+    def __str__(self):
+        return f"(mod {self.args[1]} {self.args[0]})"
 
 
 class Reciprocal(UnaryOp):
-    def op(self, x):
-        return 1/x
-    
-    def __str__(self):
-        return f"(1/ {self.args})"
+    def op(self, *args):
+        return 1/args[0]
 
+    def __str__(self):
+        return f"(1/ {self.args[0]})"
 
 
 class AbsoluteValue(UnaryOp):
-    def op(self, x):
-        return abs(x)
-    
+    def op(self, *args):
+        return abs(args[0])
+
     def __str__(self):
-        return f"abs {self.args}"
+        return f"(abs {self.args[0]})"
 
 
 d = {"+": Addition, "*": Multiplication, "**": Power, "-": Subtraction,
@@ -192,13 +184,9 @@ example = "2 3 + x * 6 5 - / abs 2 ** y 1/ + 1/"
 # example = "2 3 +"
 e = Expression.from_program(example, d)
 print(e)
-# res = e.evaluate({"x": 3, "y": 7})
-# print(res)
+res = e.evaluate({"x": 3, "y": 7})
+print(res)
 
 # Ouput atteso:
 # (1/ (+ (1/ y) (** 2 (abs (/ (- 5 6) (* x (+ 3 2)))))))
 # 0.84022932953024
-
-# Note:
-# possiamo usare la conversione float o int per verificare se abbiamo un numero, una variabile o una operazione
-# volendo come cosa aggiuntiva possiamo generare un file c che poi possiamo compilare
